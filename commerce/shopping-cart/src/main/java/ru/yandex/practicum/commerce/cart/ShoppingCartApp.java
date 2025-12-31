a4498b75cec6b6b7cc2234e1609362277254e7d0
Антон Лучик <Intemic@yandex.ru>
2025-12-31 14:41:41 +0500

    Change annotation ShoppingCartApp

--- commerce/shopping-cart/src/main/java/ru/yandex/practicum/commerce/cart/ShoppingCartApp.java
+++ commerce/shopping-cart/src/main/java/ru/yandex/practicum/commerce/cart/ShoppingCartApp.java
@@ -2,9 +2,10 @@ package ru.yandex.practicum.commerce.cart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "ru.yandex.practicum.commerce.interaction.api")
 @EnableFeignClients(basePackages = "ru.yandex.practicum.commerce.interaction.api.client")
 public class ShoppingCartApp {
     public static void main(String[] args) {
